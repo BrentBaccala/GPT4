9e56a8e3f16026c05a3694f4c33145e82899003c
Brent Baccala <cosine@freesoft.org>
2023-06-12 12:31:54 -0400

    still working on getting the logging right on construct_s_pair

--- input2.c
+++ input2.c
@@ -11,51 +11,46 @@ void reduce_by_vector(fmpz_mpoly_t poly, const fmpz_mpoly_vec_t vec, const fmpz_
 void buchberger_naive(fmpz_mpoly_vec_t res, const fmpz_mpoly_vec_t gens, const fmpz_mpoly_ctx_t ctx);
 
 void construct_s_pair(fmpz_mpoly_t s_pair, const fmpz_mpoly_t poly1, const fmpz_mpoly_t poly2, const fmpz_mpoly_ctx_t ctx) {
-    // Log the start of the construction
-    fprintf(stderr, "Constructing s-pair for:\n");
-    char **vars = {"x", "y", "z"};  // adjust according to the number of variables in your polynomial ring
-    char *poly1_str = fmpz_mpoly_get_str_pretty(poly1, vars, ctx);
-    char *poly2_str = fmpz_mpoly_get_str_pretty(poly2, vars, ctx);
-    fprintf(stderr, "Poly1: %s\n", poly1_str);
-    fprintf(stderr, "Poly2: %s\n", poly2_str);
-    flint_free(poly1_str);
-    flint_free(poly2_str);
+    fmpz_mpoly_t lt1, lt2, gcd, temp1, temp2;
+    char **vars;
 
-    // Construct the leading terms of the input polynomials
-    fmpz_mpoly_t lt1, lt2;
+    fprintf(stderr, "Beginning construction of the s-pair\n");
+
+    vars = (char **)malloc(ctx->minfo->nvars * sizeof(char *));
+    for (int i = 0; i < ctx->minfo->nvars; i++) {
+        vars[i] = (char *)malloc(2 * sizeof(char));
+        snprintf(vars[i], 2, "x%d", i + 1);
+    }
+
     fmpz_mpoly_init(lt1, ctx);
     fmpz_mpoly_init(lt2, ctx);
+    fmpz_mpoly_init(gcd, ctx);
+    fmpz_mpoly_init(temp1, ctx);
+    fmpz_mpoly_init(temp2, ctx);
+
     fmpz_mpoly_leadterm(lt1, poly1, ctx);
     fmpz_mpoly_leadterm(lt2, poly2, ctx);
 
-    // Construct the GCD of the leading terms
-    fmpz_mpoly_t gcd;
-    fmpz_mpoly_init(gcd, ctx);
     fmpz_mpoly_gcd(gcd, lt1, lt2, ctx);
 
-    // Multiply a copy of each input polynomial by the leading term of the other input polynomial
-    fmpz_mpoly_t poly1_copy, poly2_copy;
-    fmpz_mpoly_init(poly1_copy, ctx);
-    fmpz_mpoly_init(poly2_copy, ctx);
-    fmpz_mpoly_mul(poly1_copy, poly1, lt2, ctx);
-    fmpz_mpoly_mul(poly2_copy, poly2, lt1, ctx);
+    fmpz_mpoly_mul(temp1, poly1, lt2, ctx);
+    fmpz_mpoly_divexact(temp1, temp1, gcd, ctx);
 
-    // Divide each copy by the GCD
-    fmpz_mpoly_div(poly1_copy, poly1_copy, gcd, ctx);
-    fmpz_mpoly_div(poly2_copy, poly2_copy, gcd, ctx);
+    fmpz_mpoly_mul(temp2, poly2, lt1, ctx);
+    fmpz_mpoly_divexact(temp2, temp2, gcd, ctx);
 
-    // Subtract the two copies to get the result
-    fmpz_mpoly_sub(s_pair, poly1_copy, poly2_copy, ctx);
+    fmpz_mpoly_sub(s_pair, temp1, temp2, ctx);
 
-    // Log the constructed s-pair
-    char *s_pair_str = fmpz_mpoly_get_str_pretty(s_pair, vars, ctx);
-    fprintf(stderr, "Constructed s-pair: %s\n", s_pair_str);
-    flint_free(s_pair_str);
+    fprintf(stderr, "Constructed s-pair for %s and %s\n", fmpz_mpoly_get_str_pretty(poly1, vars, ctx), fmpz_mpoly_get_str_pretty(poly2, vars, ctx));
 
-    // Clean up
+    for (int i = 0; i < ctx->minfo->nvars; i++) {
+        free(vars[i]);
+    }
+    free(vars);
+
     fmpz_mpoly_clear(lt1, ctx);
     fmpz_mpoly_clear(lt2, ctx);
     fmpz_mpoly_clear(gcd, ctx);
-    fmpz_mpoly_clear(poly1_copy, ctx);
-    fmpz_mpoly_clear(poly2_copy, ctx);
+    fmpz_mpoly_clear(temp1, ctx);
+    fmpz_mpoly_clear(temp2, ctx);
 }
