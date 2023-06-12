0b3d9b0844c597b8c13dad950814b2bfce03908a
Brent Baccala <cosine@freesoft.org>
2023-06-12 13:15:51 -0400

    it's trying to log in reduce_by_vector, but has screwed up the variable names (understandably, needs more instruction) and it can't compile again

--- input1.c
+++ input1.c
@@ -10,37 +10,47 @@ void construct_s_pair(fmpz_mpoly_t s_pair, const fmpz_mpoly_t poly1, const fmpz_
 void reduce_by_vector(fmpz_mpoly_t poly, const fmpz_mpoly_vec_t vec, const fmpz_mpoly_ctx_t ctx);
 void buchberger_naive(fmpz_mpoly_vec_t res, const fmpz_mpoly_vec_t gens, const fmpz_mpoly_ctx_t ctx);
 
-
 void reduce_by_vector(fmpz_mpoly_t poly, const fmpz_mpoly_vec_t vec, const fmpz_mpoly_ctx_t ctx) {
-    slong i;
-    int is_reducible;
-    fmpz_mpoly_t leadterm, divisor, gcd, temp;
-    
-    fmpz_mpoly_init(leadterm, ctx);
-    fmpz_mpoly_init(divisor, ctx);
-    fmpz_mpoly_init(gcd, ctx);
-    fmpz_mpoly_init(temp, ctx);
+    fmpz_mpoly_t leadterm_poly, temp_poly, gcd_poly, vec_poly;
 
+    fmpz_mpoly_init(leadterm_poly, ctx);
+    fmpz_mpoly_init(temp_poly, ctx);
+    fmpz_mpoly_init(gcd_poly, ctx);
+    fmpz_mpoly_init(vec_poly, ctx);
+
+    char **var_strs = (char *[]){"x", "y", "z"}; // You can modify this to match the number of variables in your polynomial ring
+    char *poly_str = fmpz_mpoly_get_str_pretty(poly, var_strs, ctx);
+    fprintf(stderr, "Input polynomial: %s\n", poly_str);
+    flint_free(poly_str);
+
+    int reduced;
     do {
-        is_reducible = 0;
-        fmpz_mpoly_leadterm(leadterm, poly, ctx);
+        reduced = 0;
+        for (slong i = 0; i < vec->length; i++) {
+            fmpz_mpoly_set(vec_poly, fmpz_mpoly_vec_entry(vec, i), ctx);
+            fmpz_mpoly_leadterm(leadterm_poly, vec_poly, ctx);
+            
+            if (fmpz_mpoly_divides(temp_poly, poly, leadterm_poly, ctx)) {
+                fmpz_mpoly_gcd(gcd_poly, leadterm_poly, temp_poly, ctx);
+                fmpz_mpoly_mul(temp_poly, vec_poly, gcd_poly, ctx);
+                fmpz_mpoly_sub(poly, poly, temp_poly, ctx);
 
-        for (i = 0; i < vec->length; i++) {
-            fmpz_mpoly_set(divisor, fmpz_mpoly_vec_entry(vec, i), ctx);
-            fmpz_mpoly_leadterm(gcd, divisor, ctx);
+                poly_str = fmpz_mpoly_get_str_pretty(poly, var_strs, ctx);
+                fprintf(stderr, "Reduced by vector polynomial %ld: %s\n", i, poly_str);
+                flint_free(poly_str);
 
-            if (fmpz_mpoly_divides(gcd, leadterm, gcd, ctx)) {
-                is_reducible = 1;
-                fmpz_mpoly_divexact(temp, leadterm, gcd, ctx);
-                fmpz_mpoly_mul(temp, temp, divisor, ctx);
-                fmpz_mpoly_sub(poly, poly, temp, ctx);
+                reduced = 1;
                 break;
             }
         }
-    } while (is_reducible && !fmpz_mpoly_is_zero(poly, ctx));
+    } while (reduced && !fmpz_mpoly_is_zero(poly, ctx));
 
-    fmpz_mpoly_clear(leadterm, ctx);
-    fmpz_mpoly_clear(divisor, ctx);
-    fmpz_mpoly_clear(gcd, ctx);
-    fmpz_mpoly_clear(temp, ctx);
+    poly_str = fmpz_mpoly_get_str_pretty(poly, var_strs, ctx);
+    fprintf(stderr, "Result after reduction: %s\n", poly_str);
+    flint_free(poly_str);
+
+    fmpz_mpoly_clear(leadterm_poly, ctx);
+    fmpz_mpoly_clear(temp_poly, ctx);
+    fmpz_mpoly_clear(gcd_poly, ctx);
+    fmpz_mpoly_clear(vec_poly, ctx);
 }
