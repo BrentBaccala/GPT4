379ea6bc857f65f432ab27c897ea979e96083208
Brent Baccala <cosine@freesoft.org>
2023-06-12 13:12:53 -0400

    compiles again, but no logging in reduce_by_vector

--- input1.c
+++ input1.c
@@ -10,35 +10,37 @@ void construct_s_pair(fmpz_mpoly_t s_pair, const fmpz_mpoly_t poly1, const fmpz_
 void reduce_by_vector(fmpz_mpoly_t poly, const fmpz_mpoly_vec_t vec, const fmpz_mpoly_ctx_t ctx);
 void buchberger_naive(fmpz_mpoly_vec_t res, const fmpz_mpoly_vec_t gens, const fmpz_mpoly_ctx_t ctx);
 
-void reduce_by_vector(fmpz_mpoly_t poly, const fmpz_mpoly_vec_t vec, const fmpz_mpoly_ctx_t ctx) {
-    fmpz_mpoly_t lead_poly, divisor, temp;
-    int reduction_occurred;
 
-    fmpz_mpoly_init(lead_poly, ctx);
+void reduce_by_vector(fmpz_mpoly_t poly, const fmpz_mpoly_vec_t vec, const fmpz_mpoly_ctx_t ctx) {
+    slong i;
+    int is_reducible;
+    fmpz_mpoly_t leadterm, divisor, gcd, temp;
+    
+    fmpz_mpoly_init(leadterm, ctx);
     fmpz_mpoly_init(divisor, ctx);
+    fmpz_mpoly_init(gcd, ctx);
     fmpz_mpoly_init(temp, ctx);
 
-    fprintf(stderr, "Input polynomial: %s\n", fmpz_mpoly_get_str_pretty(poly, NULL, ctx));
-
     do {
-        reduction_occurred = 0;
-        for (slong i = 0; i < vec->length && !fmpz_mpoly_is_zero(poly, ctx); i++) {
-            fmpz_mpoly_t current_poly = fmpz_mpoly_vec_entry(vec, i);
-            fmpz_mpoly_leadterm(lead_poly, current_poly, ctx);
+        is_reducible = 0;
+        fmpz_mpoly_leadterm(leadterm, poly, ctx);
 
-            if (fmpz_mpoly_divides(temp, poly, lead_poly, ctx)) {
-                fprintf(stderr, "Reducing by %s\n", fmpz_mpoly_get_str_pretty(current_poly, NULL, ctx));
-                fmpz_mpoly_mul(temp, temp, current_poly, ctx);
+        for (i = 0; i < vec->length; i++) {
+            fmpz_mpoly_set(divisor, fmpz_mpoly_vec_entry(vec, i), ctx);
+            fmpz_mpoly_leadterm(gcd, divisor, ctx);
+
+            if (fmpz_mpoly_divides(gcd, leadterm, gcd, ctx)) {
+                is_reducible = 1;
+                fmpz_mpoly_divexact(temp, leadterm, gcd, ctx);
+                fmpz_mpoly_mul(temp, temp, divisor, ctx);
                 fmpz_mpoly_sub(poly, poly, temp, ctx);
-                reduction_occurred = 1;
                 break;
             }
         }
-    } while (reduction_occurred);
+    } while (is_reducible && !fmpz_mpoly_is_zero(poly, ctx));
 
-    fprintf(stderr, "Reduced polynomial: %s\n", fmpz_mpoly_get_str_pretty(poly, NULL, ctx));
-
-    fmpz_mpoly_clear(lead_poly, ctx);
+    fmpz_mpoly_clear(leadterm, ctx);
     fmpz_mpoly_clear(divisor, ctx);
+    fmpz_mpoly_clear(gcd, ctx);
     fmpz_mpoly_clear(temp, ctx);
 }
