7b0d2908fbfa018301398c5c92d8876299e0edbd
Brent Baccala <cosine@freesoft.org>
2023-06-12 13:48:55 -0400

    it compiles again

--- input1.c
+++ input1.c
@@ -11,26 +11,26 @@ void reduce_by_vector(fmpz_mpoly_t poly, const fmpz_mpoly_vec_t vec, const fmpz_
 void buchberger_naive(fmpz_mpoly_vec_t res, const fmpz_mpoly_vec_t gens, const fmpz_mpoly_ctx_t ctx);
 
 void reduce_by_vector(fmpz_mpoly_t poly, const fmpz_mpoly_vec_t vec, const fmpz_mpoly_ctx_t ctx) {
-    slong i;
-    int changed;
-    fmpz_mpoly_t leadterm, temp_poly, gcd_term;
+    fmpz_mpoly_t leadterm, gcd_leadterm, temp_poly;
+    int found_divisor;
+    slong i, nvars;
+    char **varnames;
 
-    char **varnames = (char **) flint_malloc(ctx->minfo->nvars * sizeof(char *));
-    for (i = 0; i < ctx->minfo->nvars; i++) {
-        varnames[i] = (char *) flint_malloc(16 * sizeof(char));
-        flint_sprintf(varnames[i], "x%wd", i + 1);
+    nvars = ctx->minfo->nvars;
+    varnames = (char **) flint_malloc(nvars * sizeof(char *));
+    for (i = 0; i < nvars; i++) {
+        varnames[i] = (char *) flint_malloc(10 * sizeof(char));
+        sprintf(varnames[i], "x%ld", i + 1);
     }
 
-    char *poly_str = fmpz_mpoly_get_str_pretty(poly, (const char **) varnames, ctx);
-    flint_fprintf(stderr, "Input polynomial: %s\n", poly_str);
-    flint_free(poly_str);
+    fprintf(stderr, "Input: %s\n", fmpz_mpoly_get_str_pretty(poly, (const char **) varnames, ctx));
 
     fmpz_mpoly_init(leadterm, ctx);
+    fmpz_mpoly_init(gcd_leadterm, ctx);
     fmpz_mpoly_init(temp_poly, ctx);
-    fmpz_mpoly_init(gcd_term, ctx);
 
     do {
-        changed = 0;
+        found_divisor = 0;
         fmpz_mpoly_leadterm(leadterm, poly, ctx);
 
         for (i = 0; i < vec->length; i++) {
@@ -38,34 +38,28 @@ void reduce_by_vector(fmpz_mpoly_t poly, const fmpz_mpoly_vec_t vec, const fmpz_
             fmpz_mpoly_init(vec_poly, ctx);
             fmpz_mpoly_set(vec_poly, fmpz_mpoly_vec_entry(vec, i), ctx);
 
-            if (fmpz_mpoly_divides(temp_poly, leadterm, fmpz_mpoly_vec_entry(vec, i), ctx)) {
-                fmpz_mpoly_gcd(gcd_term, leadterm, fmpz_mpoly_vec_entry(vec, i), ctx);
-                fmpz_mpoly_mul(temp_poly, vec_poly, gcd_term, ctx);
+            if (fmpz_mpoly_divides(gcd_leadterm, leadterm, vec_poly, ctx)) {
+                found_divisor = 1;
+                fprintf(stderr, "Divisor found: %s\n", fmpz_mpoly_get_str_pretty(vec_poly, (const char **) varnames, ctx));
+
+                fmpz_mpoly_mul(temp_poly, vec_poly, gcd_leadterm, ctx);
                 fmpz_mpoly_sub(poly, poly, temp_poly, ctx);
-
-                poly_str = fmpz_mpoly_get_str_pretty(poly, (const char **) varnames, ctx);
-                flint_fprintf(stderr, "Reduced by %wd-th polynomial: %s\n", i, poly_str);
-                flint_free(poly_str);
-
                 fmpz_mpoly_leadterm(leadterm, poly, ctx);
-                changed = 1;
                 break;
             }
 
-            fmpz_mpoly_clear(vec_poly);
+            fmpz_mpoly_clear(vec_poly, ctx);
         }
-    } while (changed && !fmpz_mpoly_is_zero(poly, ctx));
+    } while (found_divisor && !fmpz_mpoly_is_zero(poly, ctx));
 
-    poly_str = fmpz_mpoly_get_str_pretty(poly, (const char **) varnames, ctx);
-    flint_fprintf(stderr, "Resulting polynomial: %s\n", poly_str);
-    flint_free(poly_str);
+    fprintf(stderr, "Output: %s\n", fmpz_mpoly_get_str_pretty(poly, (const char **) varnames, ctx));
 
-    for (i = 0; i < ctx->minfo->nvars; i++) {
+    for (i = 0; i < nvars; i++) {
         flint_free(varnames[i]);
     }
     flint_free(varnames);
 
-    fmpz_mpoly_clear(leadterm);
-    fmpz_mpoly_clear(temp_poly);
-    fmpz_mpoly_clear(gcd_term);
+    fmpz_mpoly_clear(leadterm, ctx);
+    fmpz_mpoly_clear(gcd_leadterm, ctx);
+    fmpz_mpoly_clear(temp_poly, ctx);
 }
