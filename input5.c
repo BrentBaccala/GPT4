f3fff1d753a680b303627a65d8629dbfe91bf739
Brent Baccala <cosine@freesoft.org>
2023-06-12 12:52:02 -0400

    now it can write fmpz_mpoly_leadterm, standalone

---
+++ input5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "flint/flint.h"
+#include "flint/fmpz_mpoly.h"
+#include "flint/fmpz_mpoly_factor.h"
+#include "calcium/utils_flint.h"
+
+void fmpz_mpoly_leadterm(fmpz_mpoly_t res, const fmpz_mpoly_t poly, const fmpz_mpoly_ctx_t ctx) {
+    fmpz_mpoly_set(res, poly, ctx);
+    if (fmpz_mpoly_length(res, ctx) > 1) {
+        fmpz_mpoly_truncate(res, 1, ctx);
+    }
+}
