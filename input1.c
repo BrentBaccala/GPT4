88842b8856356fd56d452600fd838617abadeb82
Brent Baccala <cosine@freesoft.org>
2023-06-12 13:29:05 -0400

    fixed the last problem, but it dropped a 'const'

--- input1.c
+++ input1.c
@@ -11,61 +11,60 @@ void reduce_by_vector(fmpz_mpoly_t poly, const fmpz_mpoly_vec_t vec, const fmpz_
 void buchberger_naive(fmpz_mpoly_vec_t res, const fmpz_mpoly_vec_t gens, const fmpz_mpoly_ctx_t ctx);
 
 void reduce_by_vector(fmpz_mpoly_t poly, const fmpz_mpoly_vec_t vec, const fmpz_mpoly_ctx_t ctx) {
-    slong i, nvars;
-    char **var_names;
-    fmpz_mpoly_t lt_poly, temp_poly, gcd_term;
+    int reduced;
+    fmpz_mpoly_t temp1, temp2, lead_term;
 
-    fmpz_mpoly_init(lt_poly, ctx);
-    fmpz_mpoly_init(temp_poly, ctx);
-    fmpz_mpoly_init(gcd_term, ctx);
+    fmpz_mpoly_init(temp1, ctx);
+    fmpz_mpoly_init(temp2, ctx);
+    fmpz_mpoly_init(lead_term, ctx);
 
-    nvars = ctx->minfo->nvars;
-    var_names = (char **) malloc(nvars * sizeof(char *));
-    for (i = 0; i < nvars; i++) {
-        var_names[i] = (char *) malloc(16 * sizeof(char));
-        snprintf(var_names[i], 16, "x%ld", i + 1);
+    char **var_names = (char **)malloc(ctx->minfo->nvars * sizeof(char *));
+    for (slong i = 0; i < ctx->minfo->nvars; i++) {
+        var_names[i] = (char *)malloc(4 * sizeof(char));
+        snprintf(var_names[i], 4, "x%ld", i + 1);
     }
 
-    char *poly_str = fmpz_mpoly_get_str_pretty(poly, (const char **) var_names, ctx);
+    char *poly_str = fmpz_mpoly_get_str_pretty(poly, var_names, ctx);
     fprintf(stderr, "Input polynomial: %s\n", poly_str);
     flint_free(poly_str);
 
-    int progress;
     do {
-        progress = 0;
-        for (i = 0; i < vec->length && !fmpz_mpoly_is_zero(poly, ctx); i++) {
+        reduced = 0;
+        for (slong i = 0; i < vec->length; i++) {
             fmpz_mpoly_t vec_poly;
-            fmpz_mpoly_init_set(vec_poly, fmpz_mpoly_vec_entry(vec, i), ctx);
+            fmpz_mpoly_init(vec_poly, ctx);
+            fmpz_mpoly_set(vec_poly, fmpz_mpoly_vec_entry(vec, i), ctx);
 
-            fmpz_mpoly_leadterm(lt_poly, vec_poly, ctx);
-            if (fmpz_mpoly_divides(gcd_term, poly, lt_poly, ctx)) {
-                poly_str = fmpz_mpoly_get_str_pretty(vec_poly, (const char **) var_names, ctx);
-                fprintf(stderr, "Divisor found: %s\n", poly_str);
-                flint_free(poly_str);
+            fmpz_mpoly_leadterm(lead_term, vec_poly, ctx);
 
-                fmpz_mpoly_mul(temp_poly, vec_poly, gcd_term, ctx);
-                fmpz_mpoly_sub(poly, poly, temp_poly, ctx);
-                progress = 1;
+            if (fmpz_mpoly_divides(temp1, poly, lead_term, ctx)) {
+                fmpz_mpoly_gcd(temp2, lead_term, poly, ctx);
+                fmpz_mpoly_divexact(temp1, poly, temp2, ctx);
+                fmpz_mpoly_mul(temp1, temp1, vec_poly, ctx);
+                fmpz_mpoly_sub(poly, poly, temp1, ctx);
 
-                poly_str = fmpz_mpoly_get_str_pretty(poly, (const char **) var_names, ctx);
-                fprintf(stderr, "Current polynomial: %s\n", poly_str);
-                flint_free(poly_str);
+                char *vec_poly_str = fmpz_mpoly_get_str_pretty(vec_poly, var_names, ctx);
+                fprintf(stderr, "Reduced by: %s\n", vec_poly_str);
+                flint_free(vec_poly_str);
+
+                reduced = 1;
+                break;
             }
 
             fmpz_mpoly_clear(vec_poly, ctx);
         }
-    } while (progress && !fmpz_mpoly_is_zero(poly, ctx));
+    } while (reduced && !fmpz_mpoly_is_zero(poly, ctx));
 
-    poly_str = fmpz_mpoly_get_str_pretty(poly, (const char **) var_names, ctx);
+    poly_str = fmpz_mpoly_get_str_pretty(poly, var_names, ctx);
     fprintf(stderr, "Reduced polynomial: %s\n", poly_str);
     flint_free(poly_str);
 
-    fmpz_mpoly_clear(lt_poly, ctx);
-    fmpz_mpoly_clear(temp_poly, ctx);
-    fmpz_mpoly_clear(gcd_term, ctx);
-
-    for (i = 0; i < nvars; i++) {
+    for (slong i = 0; i < ctx->minfo->nvars; i++) {
         free(var_names[i]);
     }
     free(var_names);
+
+    fmpz_mpoly_clear(temp1, ctx);
+    fmpz_mpoly_clear(temp2, ctx);
+    fmpz_mpoly_clear(lead_term, ctx);
 }
