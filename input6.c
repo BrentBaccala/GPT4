0c2180b3eb70c09f73cd05a7674f2ded0d8218d4
Brent Baccala <cosine@freesoft.org>
2023-06-15 20:08:00 -0400

    input6 now compiles.  Let's see if it runs...

--- input6.c
+++ input6.c
@@ -5,40 +5,40 @@
 #include "flint/fmpz_mpoly_factor.h"
 #include "calcium/utils_flint.h"
 
-void buchberger_reduced(fmpz_mpoly_vec_t res, const fmpz_mpoly_vec_t gens, const fmpz_mpoly_ctx_t ctx) {
-    slong i, j;
-    int changed, is_zero;
-    fmpz_mpoly_t poly, reduced_poly;
-    fmpz_mpoly_vec_t temp_vec;
+void fmpz_mpoly_leadterm(fmpz_mpoly_t res, const fmpz_mpoly_t poly, const fmpz_mpoly_ctx_t ctx);
+void construct_s_pair(fmpz_mpoly_t s_pair, const fmpz_mpoly_t poly1, const fmpz_mpoly_t poly2, const fmpz_mpoly_ctx_t ctx);
+void reduce_by_vector(fmpz_mpoly_t poly, const fmpz_mpoly_vec_t vec, const fmpz_mpoly_ctx_t ctx);
+void buchberger_naive(fmpz_mpoly_vec_t res, const fmpz_mpoly_vec_t gens, const fmpz_mpoly_ctx_t ctx);
 
+void buchberger_reduced(fmpz_mpoly_vec_t res, const fmpz_mpoly_vec_t gens, const fmpz_mpoly_ctx_t ctx) {
     buchberger_naive(res, gens, ctx);
 
-    fmpz_mpoly_init(poly, ctx);
+    slong i, j;
+    fmpz_mpoly_t reduced_poly;
+    fmpz_mpoly_vec_t temp_vec;
+
     fmpz_mpoly_init(reduced_poly, ctx);
     fmpz_mpoly_vec_init(temp_vec, 0, ctx);
 
-    do {
-        changed = 0;
-        for (i = 0; i < res->length; i++) {
-            fmpz_mpoly_set(poly, fmpz_mpoly_vec_entry(res, i), ctx);
-            fmpz_mpoly_vec_set(temp_vec, res, ctx);
-            fmpz_mpoly_vec_remove_index(temp_vec, i, ctx);
+    for (i = 0; i < res->length; i++) {
+        fmpz_mpoly_set(reduced_poly, fmpz_mpoly_vec_entry(res, i), ctx);
+        reduce_by_vector(reduced_poly, res, ctx);
 
-            reduce_by_vector(reduced_poly, poly, temp_vec, ctx);
-            is_zero = fmpz_mpoly_is_zero(reduced_poly, ctx);
-
-            if (!is_zero && !fmpz_mpoly_equal(poly, reduced_poly, ctx)) {
-                fmpz_mpoly_swap(reduced_poly, fmpz_mpoly_vec_entry(res, i), ctx);
-                changed = 1;
-            } else if (is_zero) {
-                fmpz_mpoly_vec_remove_index(res, i, ctx);
-                i--;
-                changed = 1;
+        if (!fmpz_mpoly_is_zero(reduced_poly, ctx)) {
+            fmpz_mpoly_swap(fmpz_mpoly_vec_entry(res, i), reduced_poly, ctx);
+        } else {
+            for (j = 0; j < res->length; j++) {
+                if (j != i) {
+                    fmpz_mpoly_vec_append(temp_vec, fmpz_mpoly_vec_entry(res, j), ctx);
+                }
             }
+            fmpz_mpoly_vec_swap(res, temp_vec, ctx);
+            fmpz_mpoly_vec_clear(temp_vec, ctx);
+            fmpz_mpoly_vec_init(temp_vec, 0, ctx);
+            i--;
         }
-    } while (changed);
+    }
 
-    fmpz_mpoly_clear(poly, ctx);
     fmpz_mpoly_clear(reduced_poly, ctx);
     fmpz_mpoly_vec_clear(temp_vec, ctx);
 }
