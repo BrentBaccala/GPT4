f398614839971db401fcc3ace1abdfd4595f7421
Brent Baccala <cosine@freesoft.org>
2023-06-22 00:15:13 -0400

    hoping that would do it, but it doesn't

--- input6.c
+++ input6.c
@@ -5,45 +5,47 @@
 #include "flint/fmpz_mpoly_factor.h"
 #include "calcium/utils_flint.h"
 
-void fmpz_mpoly_leadterm(fmpz_mpoly_t output, const fmpz_mpoly_t input, const fmpz_mpoly_ctx_t ctx);
+void fmpz_mpoly_leadterm(fmpz_mpoly_t out, const fmpz_mpoly_t in, const fmpz_mpoly_ctx_t ctx);
+void construct_s_pair(fmpz_mpoly_t out, const fmpz_mpoly_t a, const fmpz_mpoly_t b, const fmpz_mpoly_ctx_t ctx);
+void reduce_by_vector(fmpz_mpoly_t out, const fmpz_mpoly_vec_t vec, int lead_reduction, const fmpz_mpoly_ctx_t ctx);
+void buchberger_naive(fmpz_mpoly_vec_t out, const fmpz_mpoly_vec_t in, const fmpz_mpoly_ctx_t ctx);
+void buchberger_reduced(fmpz_mpoly_vec_t out, const fmpz_mpoly_vec_t in, const fmpz_mpoly_ctx_t ctx);
 
-void construct_s_pair(fmpz_mpoly_t output, const fmpz_mpoly_t f1, const fmpz_mpoly_t f2, const fmpz_mpoly_ctx_t ctx);
-
-void reduce_by_vector(fmpz_mpoly_t poly, const fmpz_mpoly_vec_t vec, const int lead_reduction_flag, const fmpz_mpoly_ctx_t ctx);
-
-void buchberger_naive(fmpz_mpoly_vec_t output, const fmpz_mpoly_vec_t input, const fmpz_mpoly_ctx_t ctx);
-
-void buchberger_reduced(fmpz_mpoly_vec_t output, const fmpz_mpoly_vec_t input, const fmpz_mpoly_ctx_t ctx) {
-    slong i, j, n;
+void buchberger_reduced(fmpz_mpoly_vec_t out, const fmpz_mpoly_vec_t in, const fmpz_mpoly_ctx_t ctx) {
     fmpz_mpoly_vec_t basis, reduced_basis;
-    fmpz_mpoly_t temp_poly;
-
-    buchberger_naive(output, input, ctx);
+    fmpz_mpoly_vec_init(basis, 0, ctx);
     fmpz_mpoly_vec_init(reduced_basis, 0, ctx);
-    fmpz_mpoly_init(temp_poly, ctx);
 
-    for (i = 0; i < output->length; i++) {
-        fmpz_mpoly_set(temp_poly, fmpz_mpoly_vec_entry(output, i), ctx);
-        fmpz_mpoly_vec_init(basis, output->length - 1, ctx);
-        n = 0;
+    buchberger_naive(basis, in, ctx);
 
-        for (j = 0; j < output->length; j++) {
+    for (slong i = 0; i < basis->length; i++) {
+        fmpz_mpoly_t reduced_poly;
+        fmpz_mpoly_vec_t others;
+
+        fmpz_mpoly_init(reduced_poly, ctx);
+        fmpz_mpoly_vec_init(others, 0, ctx);
+
+        for (slong j = 0; j < basis->length; j++) {
             if (j != i) {
-                fmpz_mpoly_set(fmpz_mpoly_vec_entry(basis, n), fmpz_mpoly_vec_entry(output, j), ctx);
-                n++;
+                fmpz_mpoly_vec_append(others, fmpz_mpoly_vec_entry(basis, j), ctx);
             }
         }
 
-        reduce_by_vector(temp_poly, basis, 1, ctx);
+        fmpz_mpoly_set(reduced_poly, fmpz_mpoly_vec_entry(basis, i), ctx);
+        reduce_by_vector(reduced_poly, others, 0, ctx);
 
-        if (!fmpz_mpoly_is_zero(temp_poly, ctx)) {
-            fmpz_mpoly_vec_append(reduced_basis, temp_poly, ctx);
+        if (fmpz_mpoly_is_zero(reduced_poly, ctx)) {
+            fmpz_mpoly_clear(reduced_poly, ctx);
+        } else {
+            fmpz_mpoly_swap(reduced_poly, fmpz_mpoly_vec_entry(basis, i), ctx);
+            fmpz_mpoly_vec_append(reduced_basis, reduced_poly, ctx);
+            fmpz_mpoly_clear(reduced_poly, ctx);
         }
 
-        fmpz_mpoly_vec_clear(basis, ctx);
+        fmpz_mpoly_vec_clear(others, ctx);
     }
 
-    fmpz_mpoly_vec_swap(output, reduced_basis, ctx);
+    fmpz_mpoly_vec_swap(out, reduced_basis, ctx);
     fmpz_mpoly_vec_clear(reduced_basis, ctx);
-    fmpz_mpoly_clear(temp_poly, ctx);
+    fmpz_mpoly_vec_clear(basis, ctx);
 }
