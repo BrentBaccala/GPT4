04272d783cf5dac9d369b7d0612ba62062a05789
Brent Baccala <cosine@freesoft.org>
2023-06-24 21:19:52 -0400

    trying to split input1 into two prompts

--- input1.c
+++ input1.c
@@ -6,59 +6,35 @@
 #include "calcium/utils_flint.h"
 
 void fmpz_mpoly_leadterm(fmpz_mpoly_t output, const fmpz_mpoly_t input, const fmpz_mpoly_ctx_t ctx);
-void construct_s_pair(fmpz_mpoly_t output, const fmpz_mpoly_t input1, const fmpz_mpoly_t input2, const fmpz_mpoly_ctx_t ctx);
-void reduce_by_vector(fmpz_mpoly_t poly, const fmpz_mpoly_vec_t vec, int lead_reduction, const fmpz_mpoly_ctx_t ctx);
-void buchberger_naive(fmpz_mpoly_vec_t output, const fmpz_mpoly_vec_t input, const fmpz_mpoly_ctx_t ctx);
-void buchberger_reduced(fmpz_mpoly_vec_t output, const fmpz_mpoly_vec_t input, const fmpz_mpoly_ctx_t ctx);
-void reduce_by_matching_term(fmpz_mpoly_t poly, const fmpz_mpoly_t matching_term, const fmpz_mpoly_t reducer, const fmpz_mpoly_ctx_t ctx);
+void construct_s_pair(fmpz_mpoly_t output, const fmpz_mpoly_t poly1, const fmpz_mpoly_t poly2, const fmpz_mpoly_ctx_t ctx);
+void reduce_by_vector(fmpz_mpoly_t poly, const fmpz_mpoly_vec_t vec, int lead_reduce, const fmpz_mpoly_ctx_t ctx);
+void buchberger_naive(fmpz_mpoly_vec_t output, fmpz_mpoly_vec_t input, const fmpz_mpoly_ctx_t ctx);
+void buchberger_reduced(fmpz_mpoly_vec_t output, fmpz_mpoly_vec_t input, const fmpz_mpoly_ctx_t ctx);
 
-void reduce_by_matching_term(fmpz_mpoly_t poly, const fmpz_mpoly_t matching_term, const fmpz_mpoly_t reducer, const fmpz_mpoly_ctx_t ctx) {
-    fmpz_mpoly_t gcd, temp;
-    fmpz_mpoly_init(gcd, ctx);
-    fmpz_mpoly_init(temp, ctx);
-    fmpz_mpoly_gcd(gcd, matching_term, fmpz_mpoly_leadterm(temp, reducer, ctx), ctx);
-    fmpz_mpoly_mul(temp, reducer, gcd, ctx);
-    fmpz_mpoly_sub(poly, poly, temp, ctx);
-    fmpz_mpoly_clear(gcd, ctx);
-    fmpz_mpoly_clear(temp, ctx);
-}
+void reduce_by_matching_term(fmpz_mpoly_t poly, const fmpz_mpoly_t match_term, const fmpz_mpoly_t reduce_with, const fmpz_mpoly_ctx_t ctx) {
+    fmpz_mpoly_t gcd_term, temp_poly;
+    fmpz_mpoly_init(gcd_term, ctx);
+    fmpz_mpoly_init(temp_poly, ctx);
 
-void reduce_by_vector(fmpz_mpoly_t poly, const fmpz_mpoly_vec_t vec, int lead_reduction, const fmpz_mpoly_ctx_t ctx) {
-    fmpz_mpoly_t lead_term, vec_lead_term, temp;
-    fmpz_mpoly_init(lead_term, ctx);
-    fmpz_mpoly_init(vec_lead_term, ctx);
-    fmpz_mpoly_init(temp, ctx);
+    // Log the matching term
+    const char *varnames[] = {"x", "y", "z"};
+    char *match_term_str = fmpz_mpoly_get_str_pretty(match_term, varnames, ctx);
+    flint_fprintf(stderr, "Matching term: %s\n", match_term_str);
+    flint_free(match_term_str);
 
-    fmpz_mpoly_leadterm(lead_term, poly, ctx);
+    // Compute GCD of the matching term and the leading term of the polynomial to reduce with
+    fmpz_mpoly_t reduce_with_lead;
+    fmpz_mpoly_init(reduce_with_lead, ctx);
+    fmpz_mpoly_leadterm(reduce_with_lead, reduce_with, ctx);
+    fmpz_mpoly_gcd(gcd_term, match_term, reduce_with_lead, ctx);
 
-    for (slong i = 0; i < vec->length; i++) {
-        fmpz_mpoly_set(temp, fmpz_mpoly_vec_entry(vec, i), ctx);
-        fmpz_mpoly_leadterm(vec_lead_term, temp, ctx);
+    // Multiply the polynomial to reduce with by the GCD, store result in temp_poly
+    fmpz_mpoly_mul(temp_poly, reduce_with, gcd_term, ctx);
 
-        if (lead_reduction) {
-            if (fmpz_mpoly_divides(lead_term, lead_term, vec_lead_term, ctx)) {
-                reduce_by_matching_term(poly, lead_term, temp, ctx);
-                fmpz_mpoly_leadterm(lead_term, poly, ctx);
-                if (fmpz_mpoly_is_zero(poly, ctx)) {
-                    break;
-                }
-            }
-        } else {
-            for (slong j = 0; j < poly->length; j++) {
-                fmpz_mpoly_t term;
-                fmpz_mpoly_init(term, ctx);
-                fmpz_mpoly_get_term_coeff_fmpz(term, poly, j, ctx);
+    // Subtract temp_poly from the polynomial to be reduced
+    fmpz_mpoly_sub(poly, poly, temp_poly, ctx);
 
-                if (fmpz_mpoly_divides(term, term, vec_lead_term, ctx)) {
-                    reduce_by_matching_term(poly, term, temp, ctx);
-                }
-
-                fmpz_mpoly_clear(term, ctx);
-            }
-        }
-    }
-
-    fmpz_mpoly_clear(lead_term, ctx);
-    fmpz_mpoly_clear(vec_lead_term, ctx);
-    fmpz_mpoly_clear(temp, ctx);
+    fmpz_mpoly_clear(gcd_term, ctx);
+    fmpz_mpoly_clear(temp_poly, ctx);
+    fmpz_mpoly_clear(reduce_with_lead, ctx);
 }
