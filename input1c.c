12c500575fecd23ea61a114bc3ca6ef9122bab88
Brent Baccala <cosine@freesoft.org>
2023-06-25 01:19:39 -0400

    you just tell it stuff and it flat out ignores you

--- input1c.c
+++ input1c.c
@@ -5,23 +5,25 @@
 #include "flint/fmpz_mpoly_factor.h"
 #include "calcium/utils_flint.h"
 
-void fmpz_mpoly_leadterm(fmpz_mpoly_t, const fmpz_mpoly_t, const fmpz_mpoly_ctx_t);
-void construct_s_pair(fmpz_mpoly_t, const fmpz_mpoly_t, const fmpz_mpoly_t, const fmpz_mpoly_ctx_t);
-void reduce_by_vector(fmpz_mpoly_t, const fmpz_mpoly_vec_t, const int, const fmpz_mpoly_ctx_t);
-void buchberger_naive(fmpz_mpoly_vec_t, const fmpz_mpoly_vec_t, const fmpz_mpoly_ctx_t);
-void buchberger_reduced(fmpz_mpoly_vec_t, const fmpz_mpoly_vec_t, const fmpz_mpoly_ctx_t);
+void fmpz_mpoly_leadterm(fmpz_mpoly_t output, const fmpz_mpoly_t input, const fmpz_mpoly_ctx_t ctx);
+void construct_s_pair(fmpz_mpoly_t output, const fmpz_mpoly_t poly1, const fmpz_mpoly_t poly2, const fmpz_mpoly_ctx_t ctx);
+void reduce_by_vector(fmpz_mpoly_t inout, const fmpz_mpoly_vec_t polys, const int lead_reducing, const fmpz_mpoly_ctx_t ctx);
+void buchberger_naive(fmpz_mpoly_vec_t output, const fmpz_mpoly_vec_t input, const fmpz_mpoly_ctx_t ctx);
+void buchberger_reduced(fmpz_mpoly_vec_t output, const fmpz_mpoly_vec_t input, const fmpz_mpoly_ctx_t ctx);
 
-void fmpz_mpoly_get_term(fmpz_mpoly_t rop, const fmpz_mpoly_t poly, slong term_num, const fmpz_mpoly_ctx_t ctx) {
+void fmpz_mpoly_get_term(fmpz_mpoly_t output, const fmpz_mpoly_t input, ulong term_num, const fmpz_mpoly_ctx_t ctx) {
     if (term_num == 0) {
-        fmpz_mpoly_leadterm(rop, poly, ctx);
+        fmpz_mpoly_leadterm(output, input, ctx);
     } else {
-        fmpz_mpoly_t truncated_poly, diff_poly;
-        fmpz_mpoly_init(truncated_poly, ctx);
-        fmpz_mpoly_init(diff_poly, ctx);
-        fmpz_mpoly_truncate(truncated_poly, poly, term_num, ctx);
-        fmpz_mpoly_sub(diff_poly, poly, truncated_poly, ctx);
-        fmpz_mpoly_leadterm(rop, diff_poly, ctx);
-        fmpz_mpoly_clear(truncated_poly, ctx);
-        fmpz_mpoly_clear(diff_poly, ctx);
+        fmpz_mpoly_t truncated_input, temp_diff;
+        fmpz_mpoly_init(truncated_input, ctx);
+        fmpz_mpoly_init(temp_diff, ctx);
+
+        fmpz_mpoly_truncate(truncated_input, input, term_num, ctx);
+        fmpz_mpoly_sub(temp_diff, input, truncated_input, ctx);
+        fmpz_mpoly_leadterm(output, temp_diff, ctx);
+
+        fmpz_mpoly_clear(truncated_input, ctx);
+        fmpz_mpoly_clear(temp_diff, ctx);
     }
 }
